1f7bdfc12f51377445bc61895aed0f4afe14391b
Mozzarella12321 <mozzarella12321@gmail.com>
2024-03-17 14:44:01 +0400

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// TODO: postgress storage
+
+	// TODO: create table with sessions, save unique token and lifetime
+
+	// TODO: Auth, hash with salt
+
+	// TODO: /ping method
+}
